7f38166aa015b71da8bde07e294ad283d3d25ced
Nico <107031363+Nicob04@users.noreply.github.com>
2023-02-28 20:39:02 +0100

    stampa tabella
    da sistemare

--- tecnico.php
+++ tecnico.php
@@ -1,36 +1,43 @@
 
 <?php 
-$username = "u"; 
-$password = ""; 
-$database = ""; 
-$mysqli = new mysqli("localhost", $username, $password, $database); 
-$query = "SELECT * FROM richiesta where tipologia = 'hardware'";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "picciotti";
 
-echo '<table border="0" cellspacing="2" cellpadding="2"> 
-      <tr> 
-          <td> <font face="Arial">data</font> </td> 
-          <td> <font face="Arial">priorità</font> </td> 
-          <td> <font face="Arial">descrizione</font> </td> 
-          <td> <font face="Arial">ambito</font> </td> 
-          <td> <font face="Arial">tipologia</font> </td> 
-      </tr>';
-
-if ($result = $mysqli->query($query)) {
-    while ($row = $result->fetch_assoc()) {
-        $field1name = $row["col1"];
-        $field2name = $row["col2"];
-        $field3name = $row["col3"];
-        $field4name = $row["col4"];
-        $field5name = $row["col5"]; 
-
-        echo '<tr> 
-                  <td>'.$field1name.'</td> 
-                  <td>'.$field2name.'</td> 
-                  <td>'.$field3name.'</td> 
-                  <td>'.$field4name.'</td> 
-                  <td>'.$field5name.'</td> 
-              </tr>';
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);;
+    if (!$conn) {
+        die("Errore nella connessione al database!");
     }
-    $result->free();
-} 
+    
+    // recuperare dati dal database
+    $sql = "SELECT * FROM richiesta where tipologia = 'software'";
+    $risultato = mysqli_query($conn, $sql);
+    
+    // stampare la tabella
+    if (mysqli_num_rows($risultato) > 0) {
+        echo "<table>";
+        echo "<tr> <th>Data</th> <th>priorità</th> <th>descrizione</th> <th>ambito</th> <th>luogo</th> <th>tipologia</th> <th>status</th> </tr>";
+        // output dei dati di ogni riga
+        while($riga = mysqli_fetch_assoc($risultato)) {
+            echo "<tr>";
+            echo "<td>" . $riga['data'] . "</td>";
+            echo "<td>" . $riga['priorità'] . "</td>";
+            echo "<td>" . $riga['descrizione'] . "</td>";
+            echo "<td>" . $riga['ambito'] . "</td>";
+            echo "<td>" . $riga['luogo'] . "</td>";
+            echo "<td>" . $riga['tipologia'] . "</td>";
+            echo "<td>" . $riga['status'] . "</td>";
+    
+
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "0 risultati";
+    } 
+
+    // chiudere la connessione
+    mysqli_close($conn);
 ?>
