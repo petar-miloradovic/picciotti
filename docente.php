f000861fca314ea9d127136e1b2c8d3ee8d6279f
Petar <92732135+petar-miloradovic@users.noreply.github.com>
2023-03-04 19:09:31 +0100

    Update docente.php

--- docente.php
+++ docente.php
@@ -7,9 +7,7 @@ session_start();
 <!DOCTYPE html>
 <html lang="it">
 <head>
-    <!--
-        scritto da petar miloradovic
-    -->
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
